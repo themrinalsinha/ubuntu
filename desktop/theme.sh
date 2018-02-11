44fe365bdc402e97640089d63a63c9f66b18c598
Mrinal Sinha <themrinalsinha@gmail.com>
2018-02-11 21:26:07 +0530

    Improved code, fixed theme error

--- desktop/theme.sh
+++ desktop/theme.sh
@@ -26,26 +26,27 @@ check_status
 
 # INSTALLING THEME
 sudo echo -e "\n${BOLD}${YELLOW}INSTALLING THEME (vivacious dark/light)${RESET}"
-sudo xterm -e 'add-apt-repository ppa:ravefinity-project/ppa -y && apt install -y update \
-               && apt install -y vivacious-colors-gtk-dark vivacious-colors-gtk-light \
-               vivacious-unity-gtk-dark vivacious-unity-gtk-light'
+sudo add-apt-repository ppa:ravefinity-project/ppa -y && apt install -y update \ 
+&& apt install -y vivacious-colors-gtk-dark vivacious-colors-gtk-light \
+vivacious-unity-gtk-dark vivacious-unity-gtk-light
 check_status
 
 # INSTALLING ICON
 sudo echo -e "\n${BOLD}${YELLOW}INSTALLING ICON (flatbalous-blue)${RESET}"
-sudo xterm -e 'add-apt-repository ppa:noobslab/icons -y && \
-               apt -y update && apt install -y ultra-flat-icons'
+sudo add-apt-repository ppa:noobslab/icons -y && \
+apt -y update && apt install -y ultra-flat-icons
+check_status
 
 # INSTALLING UBUNTU TWEAK TOOL
 sudo echo -e "\n${BOLD}${YELLOW}INSTALLING (unity-tweak-tool)${RESET}"
-sudo xterm -e 'apt install -y unity-tweak-tool'
+sudo apt install -y unity-tweak-tool
 check_status
 
 # REMOVING ALL THE GARBAGES.
 sudo echo -e "\n${BOLD}${YELLOW}REMOVING TEMPERORY PPA${RESET}"
-sudo xterm -e 'add-apt-repository --remove ppa:ravefinity-project/ppa -y && \
-               add-apt-repository --remove ppa:noobslab/icons -y && \
-               apt -y autoremove && apt -y autoclean'
+sudo add-apt-repository --remove ppa:ravefinity-project/ppa -y && \
+add-apt-repository --remove ppa:noobslab/icons -y && \
+apt -y autoremove && apt -y autoclean
 check_status
 
 sudo echo -e "\n${BOLD}${YELLOW}<-- COMPLETED -->${RESET}"

837f0366a89202b9ad86e24767e932f200c3ee5e
Mrinal Sinha <themrinalsinha@gmail.com>
2018-04-07 09:45:56 +0530

    Added flask to server setup

--- server/setup.sh
+++ server/setup.sh
@@ -52,20 +52,21 @@ web_db_server=(
 )
 
 python_packages=(
+    'python3-pip'
     'python-dev'
     'python-numpy'
-    'python3-dev'
-    'python3-numpy'
-    'python3-selenium'
-    'python3-pip'
     'virtualenv'
-    'python3-scipy'
+    'python3-dev'
     'python3-nltk'
     'python3-lxml'
+    'python3-numpy'
+    'python3-scipy'
+    'python3-flask'
     'python3-psutil'
     'python3-urllib3'
     'python3-twisted'
     'python3-openpyxl'
+    'python3-selenium'
 )
 
 python_pip_packages=(

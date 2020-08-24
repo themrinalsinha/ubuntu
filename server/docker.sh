18bc05aeb141186a06c92644ac03ef3f9c90b5fc
Mrinal Sinha <themrinalsinha@gmail.com>
2020-08-24 17:50:32 +0530

    setting up docker and docker-compose on server

---
+++ server/docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# defining colors
+tput clear
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+RESET=`tput sgr0`
+BOLD=`tput bold`
+
+# setting up flag to check task execution
+check_status()
+{
+    if [ $? -eq 0 ]; then
+        echo " ->${GREEN} SUCCESSFUL${RESET}"
+    else
+        echo " ->${RED} FAILED${RESET}"
+    fi
+}
+
+# installing and setting up docker
+sudo echo -e "\n${BOLD}${YELLOW}Starting docker installation${RESET}\n"
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+check_status
+
+# installing and setting up docker compose
+sudo echo -e "\n${BOLD}${YELLOW}Downloading and setting docker-compose${RESET}\n"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+check_status

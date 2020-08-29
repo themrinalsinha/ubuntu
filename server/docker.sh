164fe965d1a2564b644bb23caba5edc705b34e01
Mrinal Sinha <themrinalsinha@gmail.com>
2020-08-29 21:18:44 +0530

    auto install packages

--- server/docker.sh
+++ server/docker.sh
@@ -23,8 +23,8 @@ sudo echo -e "\n${BOLD}${YELLOW}Starting docker installation${RESET}\n"
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 check_status
 
 # installing and setting up docker compose

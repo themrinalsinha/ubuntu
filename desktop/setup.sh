85486bb34bfe9e5bae0d705f5869ff44d5a3d765
Mrinal Sinha <themrinalsinha@gmail.com>
2020-09-28 20:59:45 +0530

    adding dark SDK

--- desktop/setup.sh
+++ desktop/setup.sh
@@ -91,6 +91,14 @@ sudo apt-get update
 sudo apt-get install lazygit -y
 check_status
 
+# installing dart SDK
+sudo echo -e "\n${BOLD}${YELLOW}Installing and setting up dart${RESET}\n"
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sudo apt-get update
+sudo apt-get install dart -y
+check_status
+
 # cleaning junks
 sudo echo -e "\n${BOLD}${YELLOW}Cleaning junks...${RESET}\n"
 sudo apt autoclean && sudo apt autoremove
